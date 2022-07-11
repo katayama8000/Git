dfec761b0a49f2b6816626a2306cd47e7b6682d3
katayama <tattu.0310@gmail.com>
2022-07-12 01:48:55 +0900

    🚧 なんか不具合ある

--- line.js
+++ line.js
@@ -1,33 +1,45 @@
-const axios = require("axios");
-const express = require('express')
-const app = express()
-const port = 4000
-const qs = require("querystring");
-const BASE_URL = "https://notify-api.line.me";
-const PATH = "/api/notify";
-const LINE_TOKEN = `jJjUsljDw4b9hmnL4Yz4IJSS0WAECbCL8pX7pz7UKdN`;
+const axios = require('axios');
+const querystring = require('querystring');
 
-const config = {
-    baseURL: BASE_URL,
-    url: PATH,
-    method: "post",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-        Authorization: `Bearer ${LINE_TOKEN}`
-    },
-    data: qs.stringify({
-        message: `今月の支払いは56000円です。`
-    })
+const Line = function () { };
+
+/**
+ * LINE Notifyのトークンセット
+ * @param {String} token LINE Notifyトークン
+ */
+Line.prototype.setToken = function (token) {
+    this.token = token;
 };
 
-//エラーハンドリングしっかりする
-app.get("/", async (req, res) => {
-    const response = await axios.request(config);
-    console.log(response.data.status);
-    ret = response.data.status
-    if (ret === 200){
-        res.json(response.data.status);
+/**
+ * LINE Notify実行
+ * @param {String} text メッセージ
+ */
+Line.prototype.notify = function (text) {
+    if (this.token == undefined || this.token == null) {
+        console.error('undefined token.');
+        return;
     }
-});
+    console.log(`notify message : ${text}`);
+    axios(
+        {
+            method: 'post',
+            url: 'https://notify-api.line.me/api/notify',
+            headers: {
+                Authorization: `Bearer ${this.token}`,
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            data: querystring.stringify({
+                message: text,
+            }),
+        }
+    )
+        .then(function (res) {
+            console.log(res.data);
+        })
+        .catch(function (err) {
+            console.error(err);
+        });
+};
 
-app.listen(port, () => console.log(`http://localhost:${port}/`))
+module.exports = Line;
