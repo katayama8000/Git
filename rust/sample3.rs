2a03d03b00d3f8692fdd08db783e6310dcfa61a2
katayama8000 <tattu.0310@gmail.com>
2023-05-19 12:31:22 +0900

    circle

---
+++ rust/sample3.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // say hello
+    println!("Hello, world!");
+    // 変数
+    let x = 1;
+    let y = 2;
+}
+
+// 円の面積を求める関数
+fn circle_area(radius: f64) -> f64 {
+    radius * radius * 3.14
+}
